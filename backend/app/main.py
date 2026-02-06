3de8e3bd8671fe2069a00cdffe89ccfeae6dc06e
Vincent <vincent.mick.gustafsson@gmail.com>
2026-02-06 12:05:51 +0100

    final test

--- backend/app/main.py
+++ backend/app/main.py
@@ -48,5 +48,10 @@ def read_root():
     return {"Hello": "World"}
 
 
+@app.get("/goodbye")
+def read_root():
+    return {"Goodbye": "World"}
+
+
 if __name__ == "__main__":
     uvicorn.run("app.main:app", host="0.0.0.0", port=8000, reload=True)
