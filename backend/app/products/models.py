4efdf9a9e4bd57dbb1f21974af0d91f425606911
Vidar Nilsson <vidar@vidarHP.local>
2026-02-16 10:57:03 +0100

    Added new models for shoppingcart and review tables

--- backend/app/products/models.py
+++ backend/app/products/models.py
@@ -9,6 +9,9 @@ from .schemas import (
     ProductConfigBase,
     ProductGroupBase,
     ProductImageBase,
+    ReviewBase,
+    ShoppingCartBase,
+    ShoppingCartItemBase,
     UserBase,
     VariationBase,
     VariationOptionBase,
@@ -40,6 +43,9 @@ class Product(ProductBase, table=True):
     product_group_id: int = Field(foreign_key="productgroup.id")
     product_group: "ProductGroup" = Relationship(back_populates="products")
     product_images: list["ProductImage"] = Relationship(back_populates="product")
+    shopping_cart_items: list["ShoppingCartItem"] = Relationship(
+        back_populates="product"
+    )
     variation_options: list["VariationOption"] = Relationship(
         back_populates="products", link_model=ProductConfig
     )
@@ -49,6 +55,7 @@ class ProductGroup(ProductGroupBase, table=True):
     id: int | None = Field(default=None, primary_key=True)
     category_id: int = Field(foreign_key="category.id")
     products: list["Product"] = Relationship(back_populates="product_group")
+    reviews: list["Review"] = Relationship(back_populates="product_group")
     category: "Category" = Relationship(back_populates="product_groups")
 
 
@@ -79,10 +86,35 @@ class ProductImage(ProductImageBase, table=True):
 class User(UserBase, table=True):
     id: int | None = Field(default=None, primary_key=True)
     email: str = Field(unique=True)
-    action: list["Action"] = Relationship(back_populates="user")
+    actions: list["Action"] = Relationship(back_populates="user")
+    reviews: list["Review"] = Relationship(back_populates="user")
+    shopping_cart: Optional["ShoppingCart"] = Relationship(back_populates="user")
 
 
 class Action(ActionBase, table=True):
     id: int | None = Field(default=None, primary_key=True)
     user_id: int = Field(foreign_key="user.id")
-    user: "User" = Relationship(back_populates="action")
+    user: "User" = Relationship(back_populates="actions")
+
+
+class Review(ReviewBase, table=True):
+    id: int | None = Field(default=None, primary_key=True)
+    product_group_id: int = Field(foreign_key="productgroup.id")
+    user_id: int = Field(foreign_key="user.id")
+    product_group: "ProductGroup" = Relationship(back_populates="reviews")
+    user: "User" = Relationship(back_populates="reviews")
+
+
+class ShoppingCart(ShoppingCartBase, table=True):
+    id: int | None = Field(default=None, primary_key=True)
+    user_id: int = Field(foreign_key="user.id", unique=True)
+    user: "User" = Relationship(back_populates="shopping_cart")
+    items: list["ShoppingCartItem"] = Relationship(back_populates="shopping_cart")
+
+
+class ShoppingCartItem(ShoppingCartItemBase, table=True):
+    id: int | None = Field(default=None, primary_key=True)
+    product_id: int = Field(foreign_key="product.id")
+    shopping_cart_id: int = Field(foreign_key="shoppingcart.id")
+    product: "Product" = Relationship(back_populates="shopping_cart_items")
+    shopping_cart: "ShoppingCart" = Relationship(back_populates="items")
