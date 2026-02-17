23c49612a8f8a195c3f37607797c93ad479e5cdd
vidarnilsson <vidnil-3@student.ltu.se>
2026-02-17 11:03:19 +0100

    update: index.js
    Co-authored-by: Lukaserikss0n <lukas.emil.eriksson@outlook.com>

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -1,13 +1,12 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import AppLayout from '@/views/AppLayout.vue'
+import AdminLayout from '@/layouts/AdminLayout.vue'
 import HomeView from '@/views/HomeView.vue'
 import CategoryView from '@/views/CategoryView.vue'
 import ProductView from '@/views/ProductView.vue'
 import AdminView from '@/views/admin/AdminView.vue'
 import AdminCreateProductView from '@/views/admin/AdminCreateProductView.vue'
-import VariationView from '@/views/admin/AdminVariationVíew.vue'
 import AdminCategoryView from '@/views/admin/AdminCategoryView.vue'
-import VariationOptionView from '@/views/admin/AdminVariationOptionView.vue'
 import AdminProductGroupView from '@/views/admin/AdminCreateProductGroupView.vue'
 
 const router = createRouter({
@@ -37,35 +36,30 @@ const router = createRouter({
     },
 
     {
-      path: '/admin/variations',
-      name: 'admin-variations',
-      component: VariationView,
-    },
-    {
-      path: '/admin/product-groups',
-      name: 'admin-product-groups',
-      component: AdminProductGroupView,
-    },
-    {
-      path: '/admin/variation-options',
-      name: 'admin-variation-options',
-      component: VariationOptionView,
-    },
-
-    {
-      path: '/admin/products',
-      name: 'admin-products',
-      component: AdminCreateProductView,
-    },
-    {
-      path: '/admin/categories',
-      name: 'admin-categories',
-      component: AdminCategoryView,
-    },
-    {
       path: '/admin',
-      name: 'admin',
-      component: AdminView,
+      component: AdminLayout,
+      children: [
+        {
+          path: '',
+          name: 'admin',
+          component: AdminView,
+        },
+        {
+          path: 'product-groups',
+          name: 'admin-product-groups',
+          component: AdminProductGroupView,
+        },
+        {
+          path: 'products',
+          name: 'admin-products',
+          component: AdminCreateProductView,
+        },
+        {
+          path: 'categories',
+          name: 'admin-categories',
+          component: AdminCategoryView,
+        },
+      ],
     },
   ],
 })
