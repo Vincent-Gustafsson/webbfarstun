3b3a05b82a61e61b574bf09155b4d82cc2e6a6a7
vidarnilsson <vidnil-3@student.ltu.se>
2026-03-10 21:42:16 +0100

    Update order.ts

--- frontend/src/types/order.ts
+++ frontend/src/types/order.ts
@@ -1,13 +1,11 @@
 export interface Order {
   id: number
   user_id: number
-  product_id: number | null
   order_nr: number
   qty: number
   unit_price: number
   line_total: number
   product_name: string
-  product_sku: string | null
   default_image: number | null
   purchased_at: string
 }
